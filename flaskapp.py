739280f71aef8ad80abb8e83bac1dfb17d73a78d
osostudionyc <schelzorion@gmail.com>
2024-09-14 17:34:41 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return '<h1>Hello World!</h1>'
+def hello_world():
+    return 'Hello World Again'
+
+#main driver function 
+
+if __name__ == '__main__':
+    app.run()
