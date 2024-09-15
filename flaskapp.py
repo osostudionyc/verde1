cbdcdc3d57b8ba27f5657734bff729142394bbc1
osostudionyc <schelzorion@gmail.com>
2024-09-15 19:02:04 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,48 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def mainindexcopy():
+
+
+    if request.method == 'POST':
+        output301 = '<br>' \
+                + '<br>' \
+                + '<a href="/webapp"> PRODUCT APP <br><br>' \
+                + '<a href="/hello"> TREE SCAN APP ' \
+                + '<br><br>' 
+        return output301
+    return '''
+    <form method="POST">
+            <br><br>
+            '<center><img src="static/deleteplastic2.png" alt="Logo" style="width:350px"> <br><br><p><i>🌿 Welcome to <b></i>DeletePlastic.io<i></b> - Eliminate Plastic from Your Life! 🌎</i>
+
+
+
+<br><br><br><br><b>
+
+<img src="static/DPdescripupdate.png" style="width:1000px">
+       
+</center>
+ </p>'
+            <br><br><center>
+             <a href="/webapp"> <img src="static/tquiz.png" style="width:300px" > <br>
+            <a href="/reviews"> <img src="static/PRZ.png" style="width:300px"></center>
+            <br><br><br><br>
+            <center><a href = "mailto: hello@deleteplastic.io"><img src="static/contact.png" style="width:300px"</img></a></center>
+            <p>
+    </form>
+    '''
+
+
+
+'''
 @app.route("/")
 def hello_world():
     return 'Hello World Again TEST'
-
+'''
 #main driver function 
 
 if __name__ == '__main__':
